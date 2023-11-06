82c9cfe391af97eaf2d32c5217a94b46a78cde11
scotttyso <tyscott@cisco.com>
2023-11-06 18:30:21 -0500

    check for count

--- classes/isight.py
+++ classes/isight.py
@@ -2178,23 +2178,27 @@ def compare_body_result(api_body, result):
                     for a,b in e.items():
                         if type(b) == dict:
                             for c,d in b.items():
-                                if not result[k][count][a][c] == d: patch_return = True
+                                if len(result) < count: patch_return = True
+                                elif not result[k][count][a][c] == d: patch_return = True
                         elif type(b) == list:
                             scount = 0
                             for s in b:
                                 if type(s) == dict:
                                     for g,h in s.items():
-                                        if not result[k][count][a][scount][g] == h: patch_return = True
+                                        if len(result) < count: patch_return = True
+                                        elif not result[k][count][a][scount][g] == h: patch_return = True
                                 scount += 1
                         else:
                             if 'Password' in a: count = count
+                            elif len(result) < count: patch_return = True
                             elif not result[k][count][a] == b: patch_return = True
                 elif type(e) == list:
                     prRed(e)
                     prRed('compare_body_result; not accounted for')
                     sys.exit(1)
                 else:
-                    if not result[k][count] == e: patch_return = True
+                    if len(result) < count: patch_return = True
+                    elif not result[k][count] == e: patch_return = True
                 count += 1
         else:
             if not result[k] == v: patch_return = True
